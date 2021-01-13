09ca021ecf4d95fbdb38610d639695193735c6cd
theiskaa <59066341+theiskaa@users.noreply.github.com>
2021-01-13 20:15:23 +0400

    create AnimatedImageFAB and example of how to use that

--- lib/fabs/image_fab.dart
+++ lib/fabs/image_fab.dart
@@ -79,7 +79,11 @@ class _ImageFABState extends State<ImageFAB> {
 
   Opacity buildButtonBodyWOpacity() {
     return Opacity(
-      opacity: isTapped ? (widget.opacityValue != null) ? widget.opacityValue : 0.5 : 1,
+      opacity: isTapped
+          ? (widget.opacityValue != null)
+              ? widget.opacityValue
+              : 0.5
+          : 1,
       child: buildButtonBody(),
     );
   }
@@ -88,6 +92,174 @@ class _ImageFABState extends State<ImageFAB> {
     return Container(
       height: (widget.size != null) ? widget.size : 60,
       width: (widget.size != null) ? widget.size : 60,
+      decoration: buildBoxDecoration(),
+      child: (widget.child != null) ? Center(child: widget.child) : Container(),
+    );
+  }
+
+  BoxDecoration buildBoxDecoration() {
+    return BoxDecoration(
+      borderRadius: (widget.borderRadius != null)
+          ? widget.borderRadius
+          : BorderRadius.circular(100),
+      border: (widget.border != null) ? widget.border : Border.all(),
+      boxShadow: (widget.shadows != null)
+          ? widget.shadows
+          : [
+              BoxShadow(
+                blurRadius: 10,
+                offset: Offset(0, 10),
+                spreadRadius: 0.2,
+                color: Colors.black.withOpacity(.2),
+              ),
+            ],
+      image: buildImage(),
+    );
+  }
+
+  DecorationImage buildImage() {
+    return DecorationImage(
+      image: widget.image,
+      onError: widget.onImageError,
+      colorFilter: widget.imageColorFilter,
+      fit: widget.imageFit,
+      alignment: (widget.imageAlignment != null)
+          ? widget.imageAlignment
+          : Alignment.center,
+      centerSlice: widget.imageCenterSlice,
+      repeat: (widget.imageRepeat != null)
+          ? widget.imageRepeat
+          : ImageRepeat.noRepeat,
+      matchTextDirection: (widget.imageMatchTextDirection != null)
+          ? widget.imageMatchTextDirection
+          : false,
+      scale: (widget.imageScale != null) ? widget.imageScale : 1.0,
+    );
+  }
+}
+
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+/// Animated Image FAB
+
+class AnimatedImageFAB extends StatefulWidget {
+  final ImageProvider<Object> image;
+  final Function onTap;
+  final Widget child;
+  final double defaultSize;
+  final double tappedSize;
+  final BorderRadiusGeometry borderRadius;
+  final BoxBorder border;
+  final List<BoxShadow> shadows;
+  final Duration duration;
+
+  ///
+  final void Function(Object, StackTrace) onImageError;
+  final ColorFilter imageColorFilter;
+  final BoxFit imageFit;
+  final AlignmentGeometry imageAlignment;
+  final Rect imageCenterSlice;
+  final ImageRepeat imageRepeat;
+  final bool imageMatchTextDirection;
+  final double imageScale;
+
+  const AnimatedImageFAB({
+    Key key,
+    @required this.image,
+    @required this.onTap,
+    this.child,
+    this.defaultSize,
+    this.tappedSize,
+    this.borderRadius,
+    this.border,
+    this.shadows,
+    this.duration,
+
+    ///
+    this.onImageError,
+    this.imageColorFilter,
+    this.imageFit,
+    this.imageAlignment,
+    this.imageCenterSlice,
+    this.imageRepeat,
+    this.imageMatchTextDirection,
+    this.imageScale,
+  }) : super(key: key);
+
+  @override
+  _AnimatedImageFABState createState() => _AnimatedImageFABState();
+}
+
+class _AnimatedImageFABState extends State<AnimatedImageFAB>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+
+  Animation _sizeAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: (widget.duration != null)
+          ? widget.duration
+          : Duration(milliseconds: 300),
+    );
+
+    _sizeAnimation = TweenSequence(
+      <TweenSequenceItem<double>>[
+        TweenSequenceItem(
+          tween: Tween<double>(
+            begin: (widget.defaultSize != null) ? widget.defaultSize : 60,
+            end: (widget.tappedSize != null) ? widget.tappedSize : 55,
+          ),
+          weight: (widget.defaultSize != null) ? widget.defaultSize : 60,
+        ),
+      ],
+    ).animate(_animationController);
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (det) {
+        _animationController.forward();
+      },
+      onTapUp: (det) {
+        _animationController.reverse();
+        widget.onTap();
+      },
+      onTapCancel: () {
+        _animationController.reverse();
+      },
+      child: buildButtonBody(),
+    );
+  }
+
+  Container buildButtonBody() {
+    return Container(
+      height: _sizeAnimation.value,
+      width: _sizeAnimation.value,
       decoration: buildBoxDecoration(),
       child: (widget.child != null) ? Center(child: widget.child) : Container(),
     );
